57a79994c30c6e97b6c67e1a5156f90470d43c99
AndreMBarreira <andremilla.am@gmail.com>
2024-07-18 21:23:30 -0300

    First commit ContosoUniversity with migration

---
+++ ContosoUniversity/Data/ContosoUniversityContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class ContosoUniversityContext : DbContext
+    {
+        public ContosoUniversityContext (DbContextOptions<ContosoUniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; } = default!;
+        public DbSet<Course> Course { get; set; } = default!;
+        public DbSet<Enrollment> Enrollment { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
