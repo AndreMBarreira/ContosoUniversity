13f670352ba4fbcbdb6467bcfc99c4eee67222ed
AndreMBarreira <andremilla.am@gmail.com>
2024-07-27 23:17:49 -0300

    data annotations

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -6,11 +7,17 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         [Display(Name = "Last Name")]
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         public string LastName { get; set; }
         [Display(Name = "First Name")]
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment>? Enrollments { get; set; }
