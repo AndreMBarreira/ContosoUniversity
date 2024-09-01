97d6715ce01ad71816fe452e9ba5837995cd3e27
AndreMBarreira <andremilla.am@gmail.com>
2024-09-01 11:16:50 -0300

    Inheritance, migration with UP method

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -3,33 +3,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        public string LastName { get; set; }
-        [Required]
-        [Display(Name = "First Name")]
-        [StringLength(50)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        [Column("FirstName")]
-        public string FirstMidName { get; set; }
         [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         public virtual ICollection<Enrollment>? Enrollments { get; set; }
     }
